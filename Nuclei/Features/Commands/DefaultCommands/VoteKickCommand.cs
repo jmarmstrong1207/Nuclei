391d837727d1566c2f2accfcf21e37b33e29aa6b
James Armstrong <jm.armstrong1207@gmail.com>
2026-01-04 04:16:25 -0800

    Improve votekick code flow - provide list of players to choose

--- Nuclei/Features/Commands/DefaultCommands/VoteKickCommand.cs
+++ Nuclei/Features/Commands/DefaultCommands/VoteKickCommand.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using BepInEx.Configuration;
+using Mirage;
 using NuclearOption.Networking;
 using Nuclei.Enums;
 using Nuclei.Helpers;
@@ -9,54 +12,67 @@ namespace Nuclei.Features.Commands.DefaultCommands;
 
 public class VoteKickCommand(ConfigFile config) : PermissionConfigurableCommand(config)
 {
+    private static List<INetworkPlayer> _playerList;
     public override string Name { get; } = "votekick";
     public override string Description { get; } = "lets you vote to kick a user using either their name or steam id.";
     public override string Usage { get; } = "votekick <name|steamid>";
     public override PermissionLevel DefaultPermissionLevel { get; } = PermissionLevel.Everyone;
-    
+
     public override bool Validate(Player player, string[] args)
     {
-        if (args.Length >= 1) return true;
-        
-        var sb = new StringBuilder();
-        sb.AppendLine("Choose a player to vote using either their name or steamid.");
-        //sb.Append("Available reasons: ");
-        //sb.Append(string.Join(", ", formattedReasons));
-        ChatService.SendPrivateChatMessage(sb.ToString(), player);
-        return false;
+        if (args.Length > 1) return false;
+        if ((args.Length == 1 && !int.TryParse(args[0], out _)) || (args.Length == 1 && int.Parse(args[0]) <= 0))
+        {
+            ChatService.SendPrivateChatMessage("Number invalid. Please Try again.", player);
+            return false;
+        }
+
+        return true;
     }
 
     public override bool Execute(Player player, string[] args)
     {
-        var target = String.Join(" ", args);
-        /*
-        var reason = args[1];
-        if(!Enum.TryParse(reason, out KickReason reasonEnum))
+        if (args.Length == 0)
         {
-            ChatService.SendPrivateChatMessage("Could not parse reason.", player);
-            return false;
-        }
-        */
-        
-        Player targetPlayer;
-
-        PlayerUtils.TryFindPlayer(target, out targetPlayer);
-        if (targetPlayer == null)
-        {
-            ChatService.SendPrivateChatMessage("Player not found.", player);
-            return false;
-        }
-        else
-        {
-            string startingMessage = $"A vote to kick {targetPlayer.PlayerName} has been started.";
-            Action action = () => Globals.NetworkManagerNuclearOptionInstance.KickPlayerAsync(targetPlayer);
-
-            if (!VoteService.StartVote(player, startingMessage, action))
+            _playerList = new List<INetworkPlayer>(Globals.AuthenticatedPlayers).Where(p => PlayerHelper.TryGetPlayer(p, out Player _)).ToList();
+            ChatService.SendPrivateChatMessage($"Choose from the following players to kick ({NucleiConfig.CommandPrefixChar}votekick <number>):", player);
+            // Get players
+            for (int i = 0; i < _playerList.Count; i++)
             {
-                ChatService.SendPrivateChatMessage("Cannot start a new votekick, please wait for current vote to expire.", player);
-                return false;
+                Player p;
+                if (PlayerUtils.TryGetPlayer(_playerList[i], out p))
+                    ChatService.SendPrivateChatMessage($"{i + 1}: {PlayerUtils.StripStaffPrefix(p.PlayerName)}", player);
             }
+            return true;
         }
+        if (_playerList == null)
+        {
+            ChatService.SendPrivateChatMessage("Please use votekick without arguments to fetch the player list first.", player);
+            return false;
+        }
+
+        int idx = int.Parse(args[0]);
+        if (idx > _playerList.Count)
+        {
+            ChatService.SendPrivateChatMessage("Number invalid. Please Try again.", player);
+            return false;
+        }
+
+        Player targetPlayer;
+        if (!PlayerUtils.TryGetPlayer(_playerList[idx - 1], out targetPlayer))
+        {
+            Nuclei.Logger?.LogError("Target player for votekick not found.");
+            return false;
+        }
+        string startingMessage = $"A vote to kick {targetPlayer.PlayerName} has been started.";
+        Action action = () => Globals.NetworkManagerNuclearOptionInstance.KickPlayerAsync(targetPlayer);
+
+        if (!VoteService.StartVote(player, startingMessage, action))
+        {
+            ChatService.SendPrivateChatMessage("Cannot start a new votekick, please wait for current vote to expire.", player);
+            return false;
+        }
+        _playerList = null;
         return true;
     }
 
