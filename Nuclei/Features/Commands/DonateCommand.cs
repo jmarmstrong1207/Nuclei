8013f889b649746d2e6f3c3b6f950202427a15d3
jmarmstrong1207 <jm.armstrong1207@gmail.com>
2026-01-05 01:23:07 -0800

    add donate command

---
+++ Nuclei/Features/Commands/DonateCommand.cs
@@ -0,0 +1,70 @@
+using BepInEx.Configuration;
+using NuclearOption.Networking;
+using Nuclei.Enums;
+using Nuclei.Helpers;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+namespace Nuclei.Features.Commands.DefaultCommands;
+
+/// <summary>
+///     Command to ban a player from the server.
+/// </summary>
+public class DonateCommand(ConfigFile config) : PermissionConfigurableCommand(config)
+{
+    public override string Name { get; } = "donate";
+    public override string Description { get; } = "Donate some of your money to somebody";
+    public override string Usage { get; } = "donate <ID in their name> <Amount in millions>. e.g: </donate 1 50> donates 50 million";
+
+    public override bool Validate(Player player, string[] args)
+    {
+        if (!int.TryParse(args[0], out _) || !int.TryParse(args[1], out _))
+        {
+            ChatService.SendPrivateChatMessage("Invalid argument(s). Please try again", player);
+            return false;
+        }
+
+        return args.Length == 2;
+    }
+
+    public override bool Execute(Player player, string[] args)
+    {
+        var idx = int.Parse(args[0]);
+        var amount = int.Parse(args[1]);
+        if (!PlayerUtils.TryFindPlayerbyID(idx, out Player? targetPlayer))
+        {
+            ChatService.SendPrivateChatMessage("Could not find player to votekick.", player);
+        }
+
+        var playerBal = player.Allocation;
+        if (amount > playerBal)
+        {
+            ChatService.SendPrivateChatMessage("You don't have that much money. Please try again", player);
+            return false;
+        }
+        
+        player.SetAllocation(player.Allocation - amount);
+        targetPlayer!.SetAllocation(targetPlayer.Allocation + amount);
+        
+        ChatService.SendChatMessage($"{player.name} has donated ${amount}m to {targetPlayer.name}");
+        return true;
+    }
+
+    public override bool Execute(string[] args)
+    {
+        var idx = int.Parse(args[0]);
+        var amount = int.Parse(args[1]);
+        if (!PlayerUtils.TryFindPlayerbyID(idx, out Player? targetPlayer))
+        {
+            Nuclei.Logger?.LogError("Could not find player to votekick.");
+            return false;
+        }
+
+        targetPlayer!.SetAllocation(targetPlayer.Allocation + amount);
+        
+        ChatService.SendChatMessage($"Server has donated ${amount}m to {targetPlayer.name}");
+        return true;
+    }
+
+
+    public override PermissionLevel DefaultPermissionLevel { get; } = PermissionLevel.Moderator;
+}
