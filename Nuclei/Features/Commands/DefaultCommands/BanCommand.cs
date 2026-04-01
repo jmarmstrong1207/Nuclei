63455480747d435167ad5e02dfb2c070f7ad983a
James Armstrong <jm.armstrong1207@gmail.com>
2026-04-01 16:19:18 -0700

    Update ban command (use TCP commands)

--- Nuclei/Features/Commands/DefaultCommands/BanCommand.cs
+++ Nuclei/Features/Commands/DefaultCommands/BanCommand.cs
@@ -1,4 +1,6 @@
+using System;
 using BepInEx.Configuration;
+using NuclearOption.DedicatedServer.Commands;
 using NuclearOption.Networking;
 using Nuclei.Enums;
 using Nuclei.Helpers;
@@ -13,51 +15,50 @@ public class BanCommand(ConfigFile config) : PermissionConfigurableCommand(confi
 {
     public override string Name { get; } = "ban";
     public override string Description { get; } = "Bans a player from the server.";
-    public override string Usage { get; } = "ban <player_name>";
+    public override string Usage { get; } = "ban <player_name> <reason>";
 
     public override bool Validate(Player player, string[] args)
     {
-        return args.Length == 1;
+        return args.Length > 1;
     }
 
     public override bool Execute(Player player, string[] args)
     {
         var target = args[0];
 
-        if (PlayerUtils.TryFindPlayer(target, out var targetPlayer))
+        int idx = int.Parse(args[0]);
+        if (!PlayerUtils.TryFindPlayerbyID(idx, out Player? targetPlayer))
         {
-            if (targetPlayer == player)
-            {
-                ChatService.SendPrivateChatMessage("You can't ban yourself.", player);
-                return false;
-            }
-
-            NucleiConfig.AddBannedPlayer(targetPlayer!.SteamID.ToString());
-            _ = Globals.NetworkManagerNuclearOptionInstance.KickPlayerAsync(targetPlayer);
-            Nuclei.Logger?.LogInfo($"Player {target} banned from the server.");
-            return true;
+            ChatService.SendPrivateChatMessage("Could not find player to votekick.", player);
+            Nuclei.Logger?.LogWarning($"Ban command run. Player [{target}] not found.");
         }
 
-        ChatService.SendPrivateChatMessage($"Player {target} not found.", player);
-        Nuclei.Logger?.LogWarning($"Player {target} not found.");
+        var reason = $"Player: {targetPlayer.PlayerName}, " + String.Join(" ", args).Substring(1);
+        CommandMessage msg = new CommandMessage();
+        msg.name = "banlist-add";
+        msg.arguments = new string[]
+        {
+            Convert.ToString(targetPlayer.SteamID), reason
+        };
+
+        if (ServerRemoteCommands.Instance.FindAndRunCommand(msg).StatusCode == StatusCode.Success)
+        {
+            ChatService.SendPrivateChatMessage($"Player {targetPlayer.PlayerName} has been banned", player);
+            Nuclei.Logger?.LogInfo($"Player {targetPlayer.PlayerName} has been banned");
+            return true;
+        }
+        else
+        {
+            ChatService.SendPrivateChatMessage($"An error has occured while attempting to ban. Report this to the server owner", player);
+            Nuclei.Logger?.LogError($"An error has occured while attempting to ban. Report this to the server owner");
+        }
+
         return false;
     }
 
     public override bool Execute(string[] args)
     {
-        var target = args[0];
-
-        if (PlayerUtils.TryFindPlayer(target, out var targetPlayer))
-        {
-
-            NucleiConfig.AddBannedPlayer(targetPlayer!.SteamID.ToString());
-            _ = Globals.NetworkManagerNuclearOptionInstance.KickPlayerAsync(targetPlayer);
-            Nuclei.Logger?.LogInfo($"Player {target} banned from the server.");
-            return true;
-        }
-
-        Nuclei.Logger?.LogWarning($"Player {target} not found.");
-        return false;
+        throw new NotImplementedException();
     }
 
 
