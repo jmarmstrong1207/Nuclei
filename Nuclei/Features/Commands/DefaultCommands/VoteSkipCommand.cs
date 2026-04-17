15d6a188f9c2ace36375f7f4959d745e39b8ab58
jmarmstrong1207 <jm.armstrong1207@gmail.com>
2026-04-17 14:17:09 -0700

    Require full majority for voteskip

--- Nuclei/Features/Commands/DefaultCommands/VoteSkipCommand.cs
+++ Nuclei/Features/Commands/DefaultCommands/VoteSkipCommand.cs
@@ -43,7 +43,7 @@ public class VoteSkipCommand(ConfigFile config) : PermissionConfigurableCommand(
                 player, 
                 Action,
                 true,
-                false
+                true
             );
         }
         else
